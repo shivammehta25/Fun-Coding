b732241edb4ea7a6337126ea89f39890b2eb068c
Shivam Mehta <shivam.mehta007@gmail.com>
2019-10-18 14:18:09 +0300

    Adding PyLinter score

--- Data Structures and Algorithms/Week_1/DC_Quick_Union_Improved_1.py
+++ Data Structures and Algorithms/Week_1/DC_Quick_Union_Improved_1.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
+"""
 ## Week 1 - Video: Quick Union Improvements
 ## This is a lazy execution of this algorithm where the parent of an element can be determined
 ## by the value present in it and we can continue to do so until the value is equal to the index
 ## at which we know that we have reached the root of that connectivity tree, what makes it better
 ## is the addition of weights, it subsiquently reduces the performance as now we can say that
 ## that depth of any node x is log_2 n
+"""
 
 class DynamicConnectivity:
-    def __init__(self, N):
+    """
+    Main Class with connected and union methods
+    """
+    def __init__(self, n):
         '''
         Complexity: n
         '''
-        self.N = N
-        self.weight = [0 for _ in range(N)]
-        self.id = [i for i in range(N)]
+        self.n = n
+        self.weight = [0 for _ in range(n)]
+        self.id = [i for i in range(n)]
         print(self.id)
 
     def root(self, p):
@@ -36,7 +41,7 @@ class DynamicConnectivity:
         '''
         first_root = self.root(p)
         second_root = self.root(q)
-        if self.weight[first_root] <= self.weight[second_root] :
+        if self.weight[first_root] <= self.weight[second_root]:
             self.id[first_root] = second_root
             self.weight[second_root] += self.weight[first_root]
         else:
@@ -44,25 +49,25 @@ class DynamicConnectivity:
             self.weight[first_root] += self.weight[second_root]
 
     def printit(self):
+        """
+        Prints the array
+        """
         print(self.id)
 
-        
 def main():
+    """
+    Main function for running the program
+    """
     total_nodes = 6
-    union_pairs = [(1,3), (2,4), (4,5)]
+    union_pairs = [(1, 3), (2, 4), (4, 5)]
     dynamic_connectivity = DynamicConnectivity(total_nodes)
-    for p,q in union_pairs:
-        dynamic_connectivity.union(p,q)
+    for p, q in union_pairs:
+        dynamic_connectivity.union(p, q)
         dynamic_connectivity.printit()
-        
 
 if __name__ == '__main__':
     main()
-
-
-
-
-# Output: 
+# Output:
 # [0, 1, 2, 3, 4, 5]
 # [0, 3, 2, 3, 4, 5]
 # [0, 3, 4, 3, 4, 5]
--- Data Structures and Algorithms/Week_1/Dynamic_connectivity_Quick_Find.py
+++ Data Structures and Algorithms/Week_1/Dynamic_connectivity_Quick_Find.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
-## Week 1 - Video : Quick Find
+""" Week 1 - Video : Quick Find
 ## This is an Eager execution of this algorithm and will perform poorly with large dataset
 ## In this we store the numbers in an array and  try to match the index of the connecting
 ## nodes, so if in an array of 5 nodes 1 and 3 are connected the array will look like
 ## [0,1,2,1,4]
+"""
 
 
 
 
 class DynamicConnectivity:
-    def __init__(self,N):
+    """
+    Class containing connected and union method for checking dynamic_connectivity
+    """
+    def __init__(self, n):
         '''
         Complexity : n
         '''
-        self.N = N
-        self.id = [i for i in range(N)]
+        self.n = n
+        self.id = [i for i in range(n)]
         print(self.id)
 
     def connected(self, p, q):
         '''
         Complexity: 1
         '''
-        return self.id[p] == self.id[q] 
+        return self.id[p] == self.id[q]
 
     def union(self, p, q):
         '''
@@ -29,29 +33,32 @@ class DynamicConnectivity:
         '''
         pid = self.id[p]
         qid = self.id[q]
-        
-        if not self.connected(p,q):
-            for i in range(self.N):
-                if self.id[i] == pid: self.id[i] = qid
+        if not self.connected(p, q):
+            for i in range(self.n):
+                if self.id[i] == pid:
+                    self.id[i] = qid
 
     def printit(self):
+        """
+        Prints the array
+        """
         print(self.id)
 
-    
 def main():
+    """
+    Main Method that runs the first time
+    """
     total_nodes = 6
-    union_pairs = [(1,3), (2,4), (4,5)]
+    union_pairs = [(1, 3), (2, 4), (4, 5)]
     dynamic_connectivity = DynamicConnectivity(total_nodes)
-    for p,q in union_pairs:
-        dynamic_connectivity.union(p,q)
+    for p, q in union_pairs:
+        dynamic_connectivity.union(p, q)
         dynamic_connectivity.printit()
 
 
 if __name__ == '__main__':
     main()
-    
-    
-## Output :
+# Output :
 ## [0, 1, 2, 3, 4, 5]
 ## [0, 3, 2, 3, 4, 5]
 ## [0, 3, 4, 3, 4, 5]
