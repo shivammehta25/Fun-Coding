ab8a1e3d511d8ae4fb1806045b756019279eb9a2
Shivam Mehta <shivam.mehta007@gmail.com>
2019-11-11 20:49:24 +0300

    Have to rewrite sieve in java or any other language for it to work proficiently

--- EDXCourseITMO/Week1/generate_tests.py
+++ EDXCourseITMO/Week1/generate_tests.py
@@ -3,58 +3,52 @@ from itertools import compress
 
 def primes(n):
     """ Returns  a list of primes < n for n > 2 """
-    sieve = bytearray([True]) * (n//2)
-    for i in range(3, int(n**0.5)+1, 2):
-        if sieve[i//2]:
-            sieve[i*i//2::i] = bytearray((n-i*i-1)//(2*i)+1)
-    return [2, *compress(range(3, n, 2), sieve[1:])]
+    factors = [1 for _ in range(n + 1)]
+    p = 3
+    max_number = 0
+    max_index = 0
+    while (p <= n):
+        for i in range(p, n + 1, p):
+            factors[i] += 1
 
+        if factors[p] > max_number:
+            max_number = factors[p]
+            max_index = p
+        p += 1
+    print(n, factors)
+    return max_index
 
-def factorization(primelist, n):
-    """ Returns a list of the prime factorization of n """
-    pf = []
-    for p in primeslist:
-        if p*p > n:
-            break
-        count = 0
-        while not n % p:
-            n //= p
-            count += 1
-        if count > 0:
-            pf.append((p, count))
-    if n > 1:
-        pf.append((n, 1))
-    return pf
-
-
-def divi(primelist, n):
-    """ Returns an unsorted list of the divisors of n """
-    divs = [1]
-    for p, e in factorization(primelist, n):
-        divs += [x*p**k for k in range(1, e+1) for x in divs]
-    return divs
-
+    # sieve = bytearray([True]) * (n // 2)
+    # for i in range(3, int(n ** 0.5) + 1, 2):
+    #     if sieve[i // 2]:
+    #         sieve[i * i // 2::i] = bytearray((n - i * i - 1) // (2 * i) + 1)
+    # print(sieve)
 
 with open('input.txt', 'r') as input:
     N = int(input.readline())
-    max_divisors = -1
-    max_number = 0
-    primeslist = primes(int(N**0.5)+1)
-    x = N//2 + 1
-    while x < N+1:
-        divisors = divi(primeslist, x)
-        print(x, divisors)
-        if len(divisors) > max_divisors:
-            max_divisors = len(divisors)
-            max_number = x
-        x += 1
+    max_number = primes(N)
     max_test = N + 1 - max_number
+    print(max_test)
 with open('output.txt', 'w') as output:
     output.write(str(max_test))
     output.write('\n')
 
 
 
+# 12 [1, 2, 4, 3, 6, 12]
+# 13 [1, 13]
+# 14 [1, 2, 7, 14]
+# 15 [1, 3, 5, 15]
+# 16 [1, 2, 4, 8, 16]
+# 17 [1, 17]
+# 18 [1, 2, 3, 6, 9, 18]
+# 19 [1, 19]
+# 20 [1, 2, 4, 5, 10, 20]
+# 21 [1, 3, 7, 21]
+# 22 [1, 2, 11, 22]
+# 23 [1, 23]
+# 24 [1, 2, 4, 8, 3, 6, 12, 24]
+# 24 : maxNumber : 24, maxTest: 1
 
 
 
