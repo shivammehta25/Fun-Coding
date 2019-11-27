1fd13d0bd56b212b03806f14a0d1151fc1d240d6
Shivam Mehta <shivam.mehta007@gmail.com>
2019-11-27 03:25:36 +0300

    Getting started with Cpp again

---
+++ CppCodes/Tutorials/first_string.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+  int a = 5, b=3;
+  string first = "Coding Cpp After a while";
+
+  cout<<"A :"<<a<<endl;
+  cout<<first;
+
+  return 0;
+}
