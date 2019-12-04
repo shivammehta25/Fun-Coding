42d5d8aa7dadc1bbdeac6cdc0a5f908c1994b3bf
Shivam Mehta <shivam.mehta007@gmail.com>
2019-12-04 03:02:32 +0300

    Working solution but timed out at 23

--- CodeSignal/Interview-Practice/LinkedList/reverseNodesKGroups.py
+++ CodeSignal/Interview-Practice/LinkedList/reverseNodesKGroups.py
@@ -9,15 +9,19 @@ def reverse_list_with_k(head, k):
     i = 0
     start = head
     prev = None
-    print('Reversse start: {}'.format(start.value))
     while i < k and head:
         temp = head.next
         head.next = prev
         prev = head
         head = temp
         i += 1
-    print('reversed start, prev: {} {}'.format(start.value, prev.value))
-    return prev, start
+        
+    if i < k:
+        prev, start, temp = reverse_list_with_k(prev, i)
+    
+    return prev, start, temp
+
+
 
 def reverseNodesInKGroups(l, k):
     if not l or k == 1 or not l.next:
@@ -25,29 +29,26 @@ def reverseNodesInKGroups(l, k):
     
     iterator = l
     head = None
-    i = 0 
     prev_last_node = None
-    while iterator:
-        i += 1
-        if i % k != 0:
-            iterator = iterator.next
-
-        else:
-            temp = iterator.next
-            print(iterator.value)
-            start, end = reverse_list_with_k(iterator, k)
-            print(start.value)
-            if i != k:
-                prev_last_node.next = start
-
-            if head is None:
-                print('here')
-
-                head = start
-            
-            prev_last_node = end
-            iterator = temp
-            
     
+
+    while iterator:
+        start, end, next_node = reverse_list_with_k(iterator, k)
+        print(f'start: {start.value}, end: {end.value}')
+        if head is None:
+            head = start
+        
+        if prev_last_node:
+            print(f'Prev node: {prev_last_node.value}')
+            prev_last_node.next = start
+        
+        
+        prev_last_node = end
+        
+        
+        iterator = next_node
+
+    prev_last_node.next = iterator
+
     return head
             
