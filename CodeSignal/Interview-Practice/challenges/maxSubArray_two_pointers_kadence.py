2d0359b67e366cd56944975fec8f4cc8128efa61
Shivam Mehta <shivam.mehta007@gmail.com>
2019-12-09 02:59:48 +0300

    Adding Introduction to algorithm

--- CodeSignal/Interview-Practice/challenges/maxSubArray_two_pointers_kadence.py
+++ CodeSignal/Interview-Practice/challenges/maxSubArray_two_pointers_kadence.py
@@ -1,4 +1,6 @@
 # https://app.codesignal.com/challenge/LrAwpTnYZR6NMCbfs
+# The Concept behind the algorithm was 
+
 def maxSubarray(inputArray):
     # Kadence algorithm
     # We use two pointers, max_current and max_
@@ -21,3 +23,16 @@ def maxSubarray(inputArray):
     
     print(start_pointer, end_pointer)
     return max_
+
+
+
+#------------------------------------------------
+# If no pointer is required one way to rewrite the code can be
+def maxSubarray(inputArray):
+    max_sum = 0
+    current_sum = 0
+    for i in range(len(inputArray)):
+        current_sum = max(0, current_sum+ inputArray[i])
+        max_sum = max(current_sum, max_sum)
+    
+    return max_sum
