06ecac5ac1396333d100975d3860e407210fbcd8
Shivam Mehta <shivam.mehta007@gmail.com>
2019-12-17 17:01:13 +0300

    Adding challenge

---
+++ CodeSignal/challenges/squareDigitsSequence.py
@@ -0,0 +1,13 @@
+def squareDigitsSequence(a0):
+    values = set()
+    values.add(a0)
+    while True:
+        sum_sq = 0  # 0
+        for i in str(a0):
+            sum_sq += int(i)**2
+        if sum_sq in values:
+            break
+        values.add(sum_sq)
+        a0 = sum_sq
+
+    return(len(values) + 1)
