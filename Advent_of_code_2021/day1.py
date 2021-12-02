9ec3f8ee9d4f15fb25d2782e530cbfacc31872b2
Shivam Mehta <shivam.mehta007@gmail.com>
2021-12-02 20:33:39 +0100

    Refractoring day 1 to a single function for general size

--- Advent_of_code_2021/day1.py
+++ Advent_of_code_2021/day1.py
@@ -9,26 +9,28 @@ def open_file(file):
     return data
 
 
-def puzzle_a(data):
+# def puzzle_a(data):
+#     x = 0
+#     for i in range(1, len(data)):
+#         x += 1 if (data[i] - data[i - 1] > 0) else 0
+
+#     print(x)
+
+
+def puzzle(data, window_size):
+
     x = 0
-    for i in range(1, len(data)):
-        x += 1 if (data[i] - data[i - 1] > 0) else 0
 
-    print(x)
+    def convolution(i):
+        return sum(data[i - window_size + 1:i + 1])
 
-
-def puzzle_b(data):
-    x = 0
-
-    def convolution(i): return sum(data[i-2:i+1])
-
-    for i in range(3, len(data)):
+    for i in range(window_size, len(data)):
         x += 1 if convolution(i) > convolution(i - 1) else 0
-
     print(x)
 
 
 if __name__ == '__main__':
     data = open_file("inputs/day1.txt")
     # puzzle_a(data)
-    puzzle_b(data)
+    puzzle(data, 1)
+    puzzle(data, 3)
